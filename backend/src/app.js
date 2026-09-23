b477c841e5e964ab38fc65a214f8912967275ee3
yashgupta-cell <yash.gupta@thoughtwin.com>
2026-09-23 12:37:54 +0530

    fix: cors issue resolved

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const cors = require("cors");
-
+const path = require('path')
 const authRoutes = require("./modules/auth/auth.routes");
 const githubRoutes = require("./modules/github/github.routes");
 const errorHandler = require("./middleware/errorHandler");
 const session = require('express-session');
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 const app = express();
+const publicPath = path.join(process.cwd(), 'public'); 
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
+
+app.use(express.static(publicPath));
 
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
 
 app.use(session({
     secret: 'gfg-key',
